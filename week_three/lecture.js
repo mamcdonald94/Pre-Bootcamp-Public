e34e41ce82e436d1a4d6ab6fec7ac10fe8e391e2
mamcdonald94 <mamcdonald94@gmail.com>
2021-05-17 21:13:45 -0400

    initial commit

---
+++ week_three/lecture.js
@@ -0,0 +1,35 @@
+// Boolean
+var firstVar = true
+var secondVar = false
+
+console.log(firstVar);
+
+// Seeing Information
+console.log(secondVar);
+
+// Numbers
+var firstNum = 13;
+var secondNum = 4.99;
+var thirdNum = -9;
+
+// Strings
+var myName = "Mark";
+
+// Concatenation
+console.log("a" + "b")
+
+// javascript can add two different data types w/o conversion //
+console.log("a" + 1);// this works, not like Python >:( //
+
+// Array
+var firstarray = [1, 2, 5, 8, 'a', 'd', 9];
+
+console.log(firstarray[0]);
+console.log(myName[2]);
+console.log(myName.charAt(2));
+
+// Objects -> Dictionaries
+var user = {"name": "Mark McDonald", "age": 26};
+            //key :  value
+console.log(user["name"]);                     
+
